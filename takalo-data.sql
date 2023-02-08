797c711a37f97fde14ee97994ef947b9c75690ac
Eriq606 <erohweltall@gmail.com>
2023-02-08 16:50:46 +0300

    Push final

--- takalo-data.sql
+++ takalo-data.sql
@@ -11,8 +11,7 @@ insert into residence values(null, 'Tana');
 insert into utilisateur values (null, 3, 'Mahefa', 'rsm-mah', '2004-03-23', 3);
 
 insert into identifiant values(null, 'root@gmail.com', 'root');
-insert into residence values(null, 'Tana');
-insert into utilisteur values(null, 'root', 'root', '2023-01-01');
+insert into utilisateur values(null, 4, 'root', 'root', '2023-01-01', 3);
 
 insert into categorie values(null, 'vetements');
 insert into categorie values(null, 'outils');
@@ -68,12 +67,3 @@ insert into imageObject values(null,9,'radio1.jfif');
 
 insert into imageObject values(null,10,'samgalax20.png');
 insert into imageObject values(null,10,'samgalax22.png');
-
-
-insert into Invitation values(null, 1, 2, 1, 3, '2005-01-12', '00:00:45');
-insert into Invitation values(null, 2, 1, 3, 1, '2005-03-15', '10:30:10');
-insert into Invitation values(null, 3, 1, 2,2, '2022-01-01', "08:08:08");
-
-insert into InvitationAccept values(1, '2005-01-12', '06:00:57');
-
-insert into InvitationRefus values (3, '2022-12-01', '15:45:00');
